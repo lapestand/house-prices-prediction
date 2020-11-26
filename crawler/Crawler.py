92ae752a90d97429e8e6a7e72d8da95d31fa0504
lapestand <lapestand@gmail.com>
2020-11-26 13:51:32 +0300

    crawler download

--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -1,6 +1,8 @@
 import time
 import os
 import requests
+import shutil
+from validator_collection import validators, checkers
 
 
 class Crawler:
@@ -8,4 +10,40 @@ class Crawler:
         self.data_sources = ["sahibinden.com"]
 
     def get_from(self, source):
-        pass
+        if source == "sahibinden.com":
+            self.from_sahibinden()
+
+    def from_sahibinden(self):
+        xml_source = "https://www.sahibinden.com/sitemap/search/real-estate.xml"
+        self.download(xml_source, "crawler/sahibinden.com/", xml_source.split('/')[-1])
+        urls = self.get_urls_from_xml(xml_source)
+
+    def get_urls_from_xml(self, xml):
+        return []
+
+    def is_xml(self, in_file):
+        if "xml" in in_file:
+            with open(in_file, 'r') as f:
+                data = re.sub(r'\s+', '', f.read())
+                if re.match(r'^<.+>$', data):
+                    return True
+        return False
+
+    def download(self, src: str, dst: str, name: str):
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+
+        dst = os.path.join(dst, name)
+
+        src = validators.url(src)
+        print(src)
+        print("indiriliyor")
+        response = requests.get(src, allow_redirects=True)
+        print("indirildi")
+        try:
+            print("kaydediliyor")
+            with open(dst, "wb") as dst_file:
+                dst_file.write(response.content)
+            print("kaydedildi")
+        except:
+            return False
