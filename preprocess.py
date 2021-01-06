e9e702aa57134a115e53a18b22d1fa8023f4406a
lapestand <lapestand@gmail.com>
2021-01-06 12:21:56 +0300

    dataset created

--- preprocess.py
+++ preprocess.py
@@ -2,4 +2,15 @@ import os
 import pandas as pd
 
 
-print(os.listdir(os.path.join("real_estate_scraper")))
+all_data = list()
+
+for file_name in os.listdir("real_estate_scraper"):
+    if file_name.endswith(".csv"):
+        data = pd.read_csv(os.path.join("real_estate_scraper", file_name), dtype="unicode")
+        print(f"File name -> {file_name}\tdataset len -> {len(data)}")
+        all_data.append(data)
+
+houses = pd.concat(all_data)
+houses = houses.drop_duplicates()
+
+houses.to_csv("dataset.csv")
